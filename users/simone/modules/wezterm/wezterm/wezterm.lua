c133d5fc2ad47e7660ca1c5f8b2d32f48d6bdd8d
sceredi <ceredi.simone.iti@gmail.com>
2024-03-07 10:01:38 +0100

    fix: changed wezterm leader key

--- users/simone/modules/wezterm/wezterm/wezterm.lua
+++ users/simone/modules/wezterm/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -19,8 +19,8 @@ config.colors = {}
 config.colors.background = "#202020"
 
 config.font = wezterm.font({
-    family = "JetBrains Mono",
-    -- harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	family = "JetBrains Mono",
+	-- harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 })
 config.font_size = 20.0
 
@@ -30,69 +30,69 @@ config.use_fancy_tab_bar = false
 -- I don't like putting anything at the ege if I can help it.
 config.enable_scroll_bar = false
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 config.tab_bar_at_bottom = true
 config.freetype_load_target = "HorizontalLcd"
 
-config.disable_default_key_bindings = true
--- Keybindings like tmux
-config.leader = { key = "a", mods = "SUPER" }
-config.leader = { key = "a", mods = "SUPER", timeout_milliseconds = 1000 }
+-- config.disable_default_key_bindings = true
+-- -- Keybindings like tmux
+config.leader = { key = "b", mods = "CTRL" }
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-    -- splitting
-    {
-        mods = "LEADER",
-        key = "-",
-        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-    },
-    {
-        mods = "LEADER",
-        key = "=",
-        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-    },
-    { key = "c", mods = "LEADER",       action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-    { key = "h", mods = "LEADER",       action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-    { key = "j", mods = "LEADER",       action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-    { key = "k", mods = "LEADER",       action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-    { key = "l", mods = "LEADER",       action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-    { key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 10 } }) },
-    { key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 10 } }) },
-    { key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 10 } }) },
-    { key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 10 } }) },
-    { key = "1", mods = "LEADER",       action = wezterm.action({ ActivateTab = 0 }) },
-    { key = "2", mods = "LEADER",       action = wezterm.action({ ActivateTab = 1 }) },
-    { key = "3", mods = "LEADER",       action = wezterm.action({ ActivateTab = 2 }) },
-    { key = "4", mods = "LEADER",       action = wezterm.action({ ActivateTab = 3 }) },
-    { key = "5", mods = "LEADER",       action = wezterm.action({ ActivateTab = 4 }) },
-    { key = "6", mods = "LEADER",       action = wezterm.action({ ActivateTab = 5 }) },
-    { key = "7", mods = "LEADER",       action = wezterm.action({ ActivateTab = 6 }) },
-    { key = "8", mods = "LEADER",       action = wezterm.action({ ActivateTab = 7 }) },
-    { key = "9", mods = "LEADER",       action = wezterm.action({ ActivateTab = 8 }) },
-    { key = "1", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 0 }) },
-    { key = "2", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 1 }) },
-    { key = "3", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 2 }) },
-    { key = "4", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 3 }) },
-    { key = "5", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 4 }) },
-    { key = "6", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 5 }) },
-    { key = "7", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 6 }) },
-    { key = "8", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 7 }) },
-    { key = "9", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 8 }) },
-    { key = "n", mods = "LEADER",       action = wezterm.action({ ActivateTabRelative = 1 }) },
-    { key = "p", mods = "LEADER",       action = wezterm.action({ ActivateTabRelative = -1 }) },
-    { key = "x", mods = "LEADER",       action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
-    { key = "f", mods = "LEADER",       action = wezterm.action_callback(sessionizer.toggle) },
-    { key = "(", mods = "LEADER",       action = wezterm.action({ SwitchWorkspaceRelative = -1 }) },
-    { key = ")", mods = "LEADER",       action = wezterm.action({ SwitchWorkspaceRelative = 1 }) },
-    { key = "s", mods = "LEADER",       action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
-    { key = "n", mods = "CTRL|SHIFT",   action = wezterm.action.SpawnWindow },
-    { key = "[", mods = "LEADER",       action = wezterm.action.ActivateCopyMode },
-    { key = "V", mods = "CTRL",         action = wezterm.action.PasteFrom("Clipboard") },
-    { key = "C", mods = "CTRL",         action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
+	-- splitting
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "=",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 10 } }) },
+	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 10 } }) },
+	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 10 } }) },
+	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 10 } }) },
+	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+	{ key = "1", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER|SHIFT", action = wezterm.action({ ActivateTab = 8 }) },
+	{ key = "n", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = 1 }) },
+	{ key = "p", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = -1 }) },
+	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	{ key = "f", mods = "LEADER", action = wezterm.action_callback(sessionizer.toggle) },
+	{ key = "(", mods = "LEADER", action = wezterm.action({ SwitchWorkspaceRelative = -1 }) },
+	{ key = ")", mods = "LEADER", action = wezterm.action({ SwitchWorkspaceRelative = 1 }) },
+	{ key = "s", mods = "LEADER", action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+	{ key = "n", mods = "CTRL|SHIFT", action = wezterm.action.SpawnWindow },
+	{ key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+	{ key = "V", mods = "CTRL", action = wezterm.action.PasteFrom("Clipboard") },
+	{ key = "C", mods = "CTRL", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
 }
 
 -- and finally, return the configuration to wezterm
