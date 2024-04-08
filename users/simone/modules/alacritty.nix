454675dd68d4561b0a4fde9747d95f8915dfe71d
sceredi <ceredi.simone.iti@gmail.com>
2024-04-08 21:44:30 +0200

    feat: kitty settings and alacritty font size

--- users/simone/modules/alacritty.nix
+++ users/simone/modules/alacritty.nix
@@ -1,15 +1,25 @@
 {
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      window = { opacity = 0.98; };
-      font = {
-        size = 14;
-        normal = {
-          family = "JetBrains Mono";
-          style = "Regular";
+  programs = {
+    alacritty = {
+      enable = true;
+      settings = {
+        window = { opacity = 0.98; };
+        font = {
+          size = 18;
+          normal = {
+            family = "JetBrains Mono";
+            style = "Regular";
+          };
         };
       };
+    };
+    kitty = {
+      enable = true;
+      font = {
+        size = 18;
+        name = "JetBrains Mono";
+      };
+      shellIntegration.mode = "no-cursor";
     };
   };
 }
