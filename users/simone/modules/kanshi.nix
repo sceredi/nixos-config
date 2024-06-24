9301b811e1d0d2a6cbf1eee25fa1b255a2b029fe
sceredi <ceredi.simone.iti@gmail.com>
2024-06-24 13:56:44 +0200

    chore: update kanshi config

--- users/simone/modules/kanshi.nix
+++ users/simone/modules/kanshi.nix
@@ -8,33 +8,39 @@
   home.packages = [ pkgs.kanshi ];
   services.kanshi = {
     enable = true;
-    profiles = {
-      undocked = {
-        outputs = [{
-          criteria = "eDP-1";
-          status = "enable";
-          mode = "2880x1800@60.001Hz";
-          position = "0,0";
-          scale = 1.25;
-        }];
-      };
-      docked = {
-        outputs = [
-          {
+    settings = [
+      {
+        profile = {
+          name = "undocked";
+          outputs = [{
             criteria = "eDP-1";
             status = "enable";
             mode = "2880x1800@60.001Hz";
-            position = "0,720";
+            position = "0,0";
             scale = 1.25;
-          }
-          {
-            criteria = "LG Electronics LG HDR 4K 0x00007E97";
-            mode = "3840x2160";
-            position = "2304,0";
-            scale = 1.0;
-          }
-        ];
-      };
-    };
+          }];
+        };
+      }
+      {
+        profile = {
+          name = "docked";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+              mode = "2880x1800@60.001Hz";
+              position = "0,720";
+              scale = 1.25;
+            }
+            {
+              criteria = "LG Electronics LG HDR 4K 0x00007E97";
+              mode = "3840x2160";
+              position = "2304,0";
+              scale = 1.0;
+            }
+          ];
+        };
+      }
+    ];
   };
 }
