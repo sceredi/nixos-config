0582bf14ebdd940b7aac47dda3b4406437eefcbd
sceredi <ceredi.simone.iti@gmail.com>
2024-09-28 19:39:55 +0200

    fix: Eratosthenes of Cyrene I don't trust you

--- modules/mixins/gammastep.nix
+++ modules/mixins/gammastep.nix
@@ -2,8 +2,8 @@
   home-manager.users.simone = { ... }: {
     services.gammastep = {
       enable = true;
-      latitude = 44.12;
-      longitude = 12.15;
+      dawnTime = "6:00-7:45";
+      duskTime = "18:35-20:15";
       temperature = {
         day = 3000;
         night = 2500;
