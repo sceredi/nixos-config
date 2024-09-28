6f186cfbaa19696a5f11500db11702af8d45d719
sceredi <ceredi.simone.iti@gmail.com>
2024-09-28 19:36:05 +0200

    chore: changed gamma levels

--- modules/mixins/gammastep.nix
+++ modules/mixins/gammastep.nix
@@ -5,8 +5,8 @@
       latitude = 44.12;
       longitude = 12.15;
       temperature = {
-        day = 4000;
-        night = 3000;
+        day = 3000;
+        night = 2500;
       };
     };
   };
--- modules/mixins/wlsunset.nix
+++ modules/mixins/wlsunset.nix
@@ -3,9 +3,9 @@
     home-manager.users.simone = { pkgs, ... }: {
       services.wlsunset = {
         enable = true;
-        latitude = "44.1267537";
-        longitude = "12.1504279";
-        temperature.day = 3000;
+        sunrise = "06.30";
+        sunset = "18:00";
+        temperature.day = 3500;
         temperature.night = 3000;
       };
     };
