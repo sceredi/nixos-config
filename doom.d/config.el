d2a91da49b3bf5dd02f925091ec163769b32ac5f
sceredi <ceredi.simone.iti@gmail.com>
2024-10-31 10:26:59 +0100

    chore: rip doom emacs in nix

--- doom.d/config.el
+++
@@ -1,9 +0,0 @@
-(setq doom-font (font-spec :family "JetBrains Mono" :size 22))
-(setq display-line-numbers-type 'relative)
-(setq org-directory "~/notes/")
-(setq org-agenda-files '("~/notes/"))
-;; PDFs visited in Org-mode are opened in Okular (and not in the default choice) https://stackoverflow.com/a/8836108/789593
-(add-hook 'org-mode-hook
-      '(lambda ()
-         (delete '("\\.pdf\\'" . default) org-file-apps)
-         (add-to-list 'org-file-apps '("\\.pdf\\'" . "okular %s"))))
