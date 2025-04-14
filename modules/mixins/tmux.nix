791591ba6325d00b457e64d9e29789fa0ad3bd76
sceredi <ceredi.simone.iti@gmail.com>
2025-04-14 21:40:00 +0200

    fix: tmux-sessionizer script

--- modules/mixins/tmux.nix
+++ modules/mixins/tmux.nix
@@ -57,18 +57,18 @@
           tmux_running=$(pgrep tmux)
 
           if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-              tmux new-session -s $selected_name -c $selected
+              tmux new-session -s "$selected_name" -c "$selected"
               exit 0
           fi
 
-          if ! tmux has-session -t=$selected_name 2> /dev/null; then
-              tmux new-session -ds $selected_name -c $selected
+          if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+              tmux new-session -ds "$selected_name" -c "$selected"
           fi
 
           if [[ -z $TMUX ]]; then
-              tmux attach -t $selected_name
+              tmux attach -t "$selected_name"
           else
-              tmux switch-client -t $selected_name
+              tmux switch-client -t "$selected_name"
           fi
         '')
       ];
