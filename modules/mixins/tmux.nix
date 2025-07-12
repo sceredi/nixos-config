f599dbecf2da71ba2e2158c52bd9cb3e62383c80
sceredi <ceredi.simone.iti@gmail.com>
2025-07-12 10:01:26 +0200

    chore: tmux stuff

--- modules/mixins/tmux.nix
+++ modules/mixins/tmux.nix
@@ -7,9 +7,29 @@
     baseIndex = 1;
 
     extraConfig = ''
+      set -g renumber-windows on  # keep numbering sequential
+      set -g repeat-time 1000  # increase "prefix-free" window
+
       set -sa terminal-overrides "*:Tc"
       set -g default-terminal "screen-256color"
-      set -g status-style 'bg=#333333 fg=#5eacd3'
+      # set -g status-style 'bg=#333333 fg=#5eacd3'
+
+      # Theme: borders
+      set -g pane-border-lines simple
+      set -g pane-border-style fg=black,bright
+      set -g pane-active-border-style fg=magenta
+
+      # Theme: status
+      set -g status-style bg=default,fg=white,bright
+      set -g status-left ""
+      set -g status-right "#[fg=white,bright]#S"
+
+      # Theme: status (windows)
+      # set -g window-status-format "●"
+      # set -g window-status-current-format "●"
+      set -g window-status-current-style "#{?window_zoomed_flag,fg=yellow,fg=magenta,nobold}"
+      set -g window-status-bell-style "fg=red,nobold"
+
       unbind C-a
       bind C-a send-prefix
 
@@ -37,36 +57,59 @@
     home.packages = with pkgs; [
       tmux-sessionizer
       (pkgs.writeShellScriptBin "tmux-sessionizer" ''
-        #!/usr/bin/env bash
+          #!/usr/bin/env bash
+
+          # Format: "path:min_depth:max_depth"
+          # - plain path for direct inclusion
+          CONFIGS=(
+          "$HOME/projects:1:1"
+          "$HOME/uni-lab:2:2"
+          "$HOME/uni-lab/PPS/2024:2:2"
+          "$HOME/.dotfiles"
+          "$HOME/.config/nvim"
+          "$HOME/exercism:2:2"
+          "$HOME/probe:2:2"
+        )
+
+        all_dirs=()
+
+        for entry in "''${CONFIGS[@]}"; do
+          if [[ "$entry" == *:*:* ]]; then
+            IFS=":" read -r base min max <<< "$entry"
+            while IFS= read -r dir; do
+              all_dirs+=("$dir")
+            done < <(fd -HI -td . "$base" --min-depth="$min" --max-depth="$max")
+          else
+            all_dirs+=("$entry")
+          fi
+        done
 
         if [[ $# -eq 1 ]]; then
-            selected=$1
+          selected=$1
         else
-            list_with_git=$(fd -HI -td ^.git$ --max-depth=7 ~/projects ~/notes ~/uni-lab ~/.dotfiles ~/.config/nvim ~/exercism ~/probe)
-            list_without_git=$(echo "$list_with_git" | awk -F'/.git/' '{print $1}')
-            selected=$(echo "$list_without_git" | fzf)
+          selected=$(printf "%s\n" "''${all_dirs[@]}" | fzf)
         fi
 
         if [[ -z $selected ]]; then
-            exit 0
+          exit 0
         fi
 
         selected_name=$(basename "$selected" | tr . _)
         tmux_running=$(pgrep tmux)
 
         if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-            tmux new-session -s "$selected_name" -c "$selected"
-            exit 0
+          tmux new-session -s "$selected_name" -c "$selected"
+          exit 0
         fi
 
         if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-            tmux new-session -ds "$selected_name" -c "$selected"
+          tmux new-session -ds "$selected_name" -c "$selected"
         fi
 
         if [[ -z $TMUX ]]; then
-            tmux attach -t "$selected_name"
+          tmux attach -t "$selected_name"
         else
-            tmux switch-client -t "$selected_name"
+          tmux switch-client -t "$selected_name"
         fi
       '')
     ];
