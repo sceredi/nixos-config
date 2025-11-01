4599a42e1837cd24463f3e76a6b864cf33b6d2de
sceredi <ceredi.simone.iti@gmail.com>
2025-11-01 10:09:21 +0100

    fix: nixos-hardware modules

--- flake.nix
+++ flake.nix
@@ -132,10 +132,10 @@
           utils.nixosModules.autoGenFromInputs
           home-manager.nixosModules.home-manager
           nix-flatpak.nixosModules.nix-flatpak
-          nixos-hardware.nixosModules.common.gpu.nvidia.pascal
-          nixos-hardware.nixosModules.common.cpu.amd
-          nixos-hardware.nixosModules.common.pc
-          nixos-hardware.nixosModules.common.pc.ssd
+          nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+          nixos-hardware.nixosModules.common-cpu-amd
+          nixos-hardware.nixosModules.common-pc
+          nixos-hardware.nixosModules.common-pc-ssd
           {
             # install the overlay
             nixpkgs.overlays = [alacritty-theme.overlays.default];
