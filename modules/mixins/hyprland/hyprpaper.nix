c9605c394ec0685617e6519ac22558d2e1db8f3f
sceredi <ceredi.simone.iti@gmail.com>
2025-12-19 21:28:40 +0100

    chore(deps): update flake

--- modules/mixins/hyprland/hyprpaper.nix
+++ modules/mixins/hyprland/hyprpaper.nix
@@ -5,10 +5,10 @@
 }: {
   services.hyprpaper = {
     enable = true;
-    package = inputs.hyprpaper.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    # package = inputs.hyprpaper.packages.${pkgs.stdenv.hostPlatform.system}.default;
     settings = {
       preload = ["$HOME/.wallpapers/wallpaper.png"];
-      wallpaper = [", $HOME/.wallpapers/wallpaper.png"];
+      wallpaper = [",$HOME/.wallpapers/wallpaper.png"];
     };
   };
 }
